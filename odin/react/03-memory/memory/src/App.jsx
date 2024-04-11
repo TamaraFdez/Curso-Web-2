2dcdba0747c7e8b353bf05845041fa675c4699d8
TamaraFdez <150025492+TamaraFdez@users.noreply.github.com>
2024-04-11 15:59:02 +0200

    Update App.jsx

--- odin/react/03-memory/memory/src/App.jsx
+++ odin/react/03-memory/memory/src/App.jsx
@@ -1,44 +1,49 @@
 import { useState, useEffect } from "react";
 import "./styles/App.css";
-import Card from "./components/Card.jsx";
 
 function App() {
-  const tags = [{},{},{},{}]
   const [isLoading, setIsLoading] = useState(true);
-  const [imagenUrl, setImagenUrl] = useState(null);
   const [error, setError] = useState(null);
-  const [board, setBoard] = useState(tags);
+  const [board, setBoard] = useState([]);
 
   useEffect(() => {
     if (isLoading) {
-      // eslint-disable-next-line no-inner-declarations
       async function fetchData() {
         try {
-          const response = await fetch(
-            `https://api.disneyapi.dev/character/${
-              Math.floor(Math.random() * (7438 - 1 + 1)) + 1
-            }`
+          const boardWithImages = await Promise.all(
+            board.map(async (item) => {
+              const response = await fetch(
+                `https://api.disneyapi.dev/character/${
+                  Math.floor(Math.random() * (7438 - 1 + 1)) + 1
+                }`
+              );
+              if (response.ok) {
+                const pj = await response.json();
+                return {
+                  ...item,
+                  imageUrl: pj.data.imageUrl,
+                  films: pj.data.films,
+                  tvShows: pj.data.tvShows,
+                };
+              } else {
+                throw new Error("Hubo un error al obtener el personaje");
+              }
+            })
           );
-          if (response.ok) {
-            const pj = await response.json();
-            setImagenUrl(pj);
-            setError(null);
-            setIsLoading(false);
-            console.log(pj.data);
-          } else {
-            setError("Hubo un error al obtener el personaje");
-          }
+          setBoard(boardWithImages);
+          setIsLoading(false);
         } catch (error) {
-          setError("No pudimos hacer la solicitud para obtener el personaje");
+          setError(error.message);
         }
       }
       fetchData();
     }
-  }, [isLoading]);
+  }, [isLoading, board]);
 
   const randomPJ = () => {
     setIsLoading(true);
   };
+
   if (error) {
     return (
       <div className="App">
@@ -47,6 +52,7 @@ function App() {
       </div>
     );
   }
+
   if (isLoading) {
     return (
       <div className="App">
@@ -54,49 +60,21 @@ function App() {
       </div>
     );
   }
-  
-// const filmsFuc =
-//     e.films.length === 0
-//       ? tvShows
-//       : films;
-  // const FillArray = (props) => {
-    
-  //   board.forEach(i){
-  //     board[i] = {
-  //       id: props.data._id,
-  //       name: props.data.name,
-  //       imageUrl: props.data.imageUrl,
-  //       tvShows: props.data.tvShows,
-  //       films: props.data.films,
-  //     }; 
-  //     setBoard() 
-  //     setIsLoading(true);
-      
-       
-  //   }
-  //   return(
-  //       <section className="Card"> 
-  //       {board.map ((e) => {
-  //        return (
-  //           <div key={e.id} className="Card">
-  //             <h2>{e.name}</h2>{" "}
-  //             <div className="imagen">
-  //               <img src={e.imageUrl} alt="Imagen Aleatoria de disney" />
-  //               <p>{e.films.length === 0 ? e.tvShows: e.films}</p>
-  //             </div>
-  //           </div>)})}
-  //       </section>
-  //     );
-  //   }
-    
- 
 
   return (
     <main className="board">
-      <h1>Memory Disney </h1>
-      {/* <FillArray imagenUrl={imagenUrl} /> */}
-      
-
+      <h1>Memory Disney</h1>
+      <section className="Card">
+        {board.map((e) => (
+          <div key={e.id} className="Card">
+            <h2>{e.name}</h2>
+            <div className="imagen">
+              <img src={e.imageUrl} alt="Imagen Aleatoria de Disney" />
+              <p>{e.films.length === 0 ? e.tvShows : e.films}</p>
+            </div>
+          </div>
+        ))}
+      </section>
       <button onClick={randomPJ}>Volver a intentarlo</button>
     </main>
   );
